2e367bc7a8666438a5cd76596c4a0a33fd869316
Francisco Santos <franciscopolaco@ist.utl.pt>
2017-08-22 21:19:54 +0100

    New Nightly

---
+++ Releases/NIGHTLY-20170822/NetworkGameCopier-NIGHTLY-20170822/InstallService.bat
@@ -0,0 +1,3 @@
+@echo OFF
+%windir%\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe UpdateService.exe
+exit
---
+++ Releases/NIGHTLY-20170822/NetworkGameCopier-NIGHTLY-20170822/UninstallService.bat
@@ -0,0 +1,3 @@
+@echo OFF
+%windir%\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /u UpdateService.exe
+exit
